a358e663db9ace3bc6846e7af3bd3e524baca33d
Daniyal Akhlaq <43551920+IMDaniyal@users.noreply.github.com>
2022-02-01 22:31:29 +0000

    User authentication setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'main/index'
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
+
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+
+  root 'main#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
